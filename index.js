f9ee95969b5edd14d02a1dd419572d8c56559c22
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-30 17:28:26 +0300

    fix: changed ticket model

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const rules = auth.rewriter({
   users: 600,
   messages: 660,
   cities: 640,
-})
+});
 
 const db = {
   users: [],
@@ -36,26 +36,28 @@ db.cities.forEach((value) => {
       if (getRandomInt(4) === 0) {
         continue;
       }
-      const departure = milSecond + (getRandomInt(24) * 3600000) + (day * 86400000);
+      const departure = milSecond + getRandomInt(24) * 3600000 + day * 86400000;
       const cost = getRandomArbitrary(60, 600);
 
       const tiket = {
         from: value,
         to: db.cities[i],
         departure: new Date(departure).toISOString(),
-        arrival: new Date(departure + (getRandomInt(7) * 3600000) + ((getRandomInt(5) + 1) * 600000)),
+        arrival: new Date(
+          departure + getRandomInt(7) * 3600000 + (getRandomInt(5) + 1) * 600000
+        ),
         price: {
-          EUR: cost,
-          USA: (cost * 1.083).toFixed(2),
-          RUB: (cost * 91.33).toFixed(2),
-          PLN: (cost * 4.56).toFixed(2),
+          EUR: Number(cost),
+          USA: Number((cost * 1.083).toFixed(2)),
+          RUB: Number((cost * 91.33).toFixed(2)),
+          PLN: Number((cost * 4.56).toFixed(2)),
         },
-        FlightNum: Math.round(getRandomArbitrary(1000, 2000)),
-        places: {
-          general: Math.round(getRandomArbitrary(50, 188)),
-          clear: Math.round(getRandomArbitrary(5, 150))
-        }
-      }
+        flightNum: Math.round(getRandomArbitrary(1000, 2000)),
+        seats: {
+          total: Math.round(getRandomArbitrary(50, 188)),
+          available: Math.round(getRandomArbitrary(5, 150)),
+        },
+      };
       db.tikets.push(tiket);
     }
   }
@@ -66,10 +68,12 @@ const PORT = '3000';
 const app = jsonServer.create();
 const router = jsonServer.router(db);
 
-app.db = router.db
-app.use(cors({
-  origin: 'http://localhost:4200'
-}));
+app.db = router.db;
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+  })
+);
 app.use(auth);
 app.use(rules);
 app.use(auth);
